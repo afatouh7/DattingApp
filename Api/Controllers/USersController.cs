77221916867c6d7ded410e391b7ac44dd99e5c45
DESKTOP-TL2G0T1\DELL G3 <fatouh_a@yahoo.com>
2022-12-23 16:08:05 +0200

    receive users

---
+++ Api/Controllers/USersController.cs
@@ -0,0 +1,32 @@
+﻿using Api.Data;
+using Api.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class USersController : ControllerBase
+    {
+        private readonly DattingContext _context;
+
+        public USersController(DattingContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+        [HttpGet("{id}")]
+        public async Task<AppUser> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user;
+        }
+    }
+}
---
+++ Api/Data/DattingContext.cs
@@ -0,0 +1,14 @@
+﻿using Api.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class DattingContext:DbContext
+    {
+        public DattingContext(DbContextOptions<DattingContext> options) : base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
